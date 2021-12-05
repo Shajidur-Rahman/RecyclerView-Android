432af30c8d1e02d432ac9ca6bebc367ec7c48576
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-12-05 10:18:27 +0600

    Finished

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+
+        ArrayList<Food> foods = new ArrayList<>();
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+        foods.add(new Food("Vatt", android.R.drawable.btn_star_big_on));
+
+        FoodAdapter foodAdapter = new FoodAdapter(this, foods);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(foodAdapter);
 
     }
 }
